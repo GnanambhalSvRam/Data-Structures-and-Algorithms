b91e3fe329efc56bf78e6ec98dafb20d94178494
Gnanambhal Shivarraman <71579776+GnanambhalSvRam@users.noreply.github.com>
2024-09-06 12:43:34 -0600

    Update 217_Contains_Duplicates.cpp

--- Arrays & Hashing /217_Contains_Duplicates.cpp
+++ Arrays & Hashing /217_Contains_Duplicates.cpp
@@ -1,1 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n=nums.size(),i;
+        unordered_map<int,int> m;
+        for(i=0;i<n;i++){
+            m[nums[i]]++;
 
+            if(m[nums[i]]>1) return true;
+        }
+        return false;
+    }
+};
