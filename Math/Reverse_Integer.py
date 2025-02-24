403e4f29bde806dde27d775214d64b08c95466c0
Gnanambhal Shivarraman <gnanambalsvram@gmail.com>
2025-02-24 09:55:25 -0700

    Update Reverse_Integer.py

--- Math/Reverse_Integer.py
+++ Math/Reverse_Integer.py
@@ -2,23 +2,16 @@ class Solution:
     def reverse(self, x: int) -> int:
         
         MAX, MIN = (2 ** 31) - 1, -1 * (2 ** 31) 
-        queue = deque()
-        neg = False
-        if x < 0:
-            neg = True
-            x = -1 * x
-
+        sign = -1 if x < 0 else 1
+        if sign == -1:
+            x = sign * x
+        
+        ans = 0
         while x > 0:
-            element = x%10
-            queue.append(element)
+            ans = ans * 10 + x%10
             x = x // 10
 
-        ans = 0
-        while queue:
-            ans = ans * 10 + queue.popleft()
-
-        if neg:
-            ans = -1 * ans
+        ans = sign * ans
 
         if ans > MAX or ans < MIN:
             ans = 0
